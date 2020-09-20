4ec9275c31ed8625cdf0ea33d595f4ae59f315f3
PeterMa <jma4@ualberta.ca>
2020-09-20 15:40:14 -0600

    change backend ip address

--- src/common/ApiUrl.js
+++ src/common/ApiUrl.js
@@ -1,9 +1,9 @@
 export default class ApiUrl {
     static PROTOCOL = 'http://';
     static SOCKET_PROTOCOL = 'ws://';
-    static BASE_SOCKET_URL =  this.SOCKET_PROTOCOL + 'localhost:3000';
+    static BASE_SOCKET_URL =  this.SOCKET_PROTOCOL + '10.162.0.4:3000';
     static BIE_7_SOCKET_URL = this.BASE_SOCKET_URL + '/bie7/';
-    static BASE_URL = this.PROTOCOL + 'localhost:3000';
+    static BASE_URL = this.PROTOCOL + '10.162.0.4:3000';
     static BIE_7_URL = this.BASE_URL + '/bie7/';
     static LOGIN_URL = this.BASE_URL + '/user/login';
     static REGISTER_URL = this.BASE_URL + '/user/register';
